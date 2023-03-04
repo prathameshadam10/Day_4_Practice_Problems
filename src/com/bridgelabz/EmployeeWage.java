f687cbbcb29604ae2543ea20b97b7e261d311513
prathameshadam10 <121919043+prathameshadam10@users.noreply.github.com>
2023-03-04 13:27:22 +0530

    Update EmployeeWage.java

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,13 +4,18 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Program");
         int emp_Full_Time = 1;
+        int emp_Part_Time = 2;
         int emp_Rate_Per_Hour = 20;
         int empHrs = 0;
         int empwage = 0;
 
 
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == emp_Full_Time) {
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == emp_Part_Time){
+            empHrs = 4;
+            System.out.println("Employee is partTime Present");
+        }
+        else if (empCheck == emp_Full_Time) {
             empHrs = 8;
             System.out.println("Employee is Present");
         } else {
@@ -20,6 +25,5 @@ public class EmployeeWage {
         empwage = empHrs * emp_Rate_Per_Hour;
         System.out.println("Emp Wage :" + empwage);
     }
-
 
 }
