84a05e6f9d08b6fbac3b44027d5c49f30c8a3885
prathameshadam10 <121919043+prathameshadam10@users.noreply.github.com>
2023-03-04 17:39:04 +0530

    Update EmployeeWage.java

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -5,42 +5,47 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.Scanner;
 
 public class EmployeeWage {
-    public static final int emp_Full_Time = 1;
-    public static final int emp_Part_Time = 2;
-    public static final int emp_Rate_Per_Hour = 20;
-    public static final  int Num_Of_working_Days = 20;
+
+        public static final int emp_Full_Time = 1;
+        public static final int emp_Part_Time = 2;
+        public static final int emp_Rate_Per_Hour = 20;
+        public static final  int Num_Of_working_Days = 20;
+        public static final int max_Hrs_In_Month = 100;
 
 
-    public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Program");
 
-        int empHrs = 0;
-        int empWage = 0;
-        int totalEmpWage = 0;
+        public static void main(String[] args) {
+            System.out.println("Welcome To Employee Wage Program");
 
-      for (int day = 0; day < EmployeeWage.Num_Of_working_Days; day++) {
-          int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int total_Working_Days = 0;
 
-          switch (empCheck) {
-              case emp_Part_Time:
-                  empHrs = 4;
-                  System.out.println("Employee is PartTime Present");
-                  break;
-              case emp_Full_Time:
-                  empHrs = 8;
-                  System.out.println("Employee is FullTime Present");
-                  break;
-              default:
-                  empHrs = 0;
-                  System.out.println("Employee is Absent");
-          }
-          empWage = empHrs * emp_Rate_Per_Hour;
-          totalEmpWage += empWage;
-          System.out.println("Emp Wage :" + empWage);
-      }
-        System.out.println("Total Emp Wage :" + totalEmpWage);
-    }
+            while (totalEmpHrs <= EmployeeWage.max_Hrs_In_Month ||
+                    total_Working_Days < EmployeeWage.Num_Of_working_Days) {
+                total_Working_Days++;
 
+                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+                switch (empCheck) {
+                    case emp_Part_Time:
+                        empHrs = 4;
+                        System.out.println("Employee is PartTime Present");
+                        break;
+                    case emp_Full_Time:
+                        empHrs = 8;
+                        System.out.println("Employee is FullTime Present");
+                        break;
+                    default:
+                        empHrs = 0;
+                        System.out.println("Employee is Absent");
+                }
+                totalEmpHrs += empHrs;
+                System.out.println("Day :" + total_Working_Days + "Emp Hr :" + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * emp_Rate_Per_Hour;
+            System.out.println("Total Emp Wage :" + totalEmpWage);
+        }
 
     }
 
