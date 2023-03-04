c85409be2a085cfd10f1668ea559caeee671432d
prathameshadam10 <prathameshadam10@gmail.com>
2023-03-04 12:25:22 +0530

    EmployeePresentAbsent

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,5 +3,11 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Program");
+        int emp_Full_Time = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == emp_Full_Time)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
